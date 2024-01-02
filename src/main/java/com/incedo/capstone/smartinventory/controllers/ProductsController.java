bad0d8e795359a64072cd5b52ec9ea8f30d9908e
sajjapushyanth <sp7173@srmist.edu.in>
2024-01-02 22:09:13 +0530

    CRUD performed for products controller and services

--- src/main/java/com/incedo/capstone/smartinventory/controllers/ProductsController.java
+++ src/main/java/com/incedo/capstone/smartinventory/controllers/ProductsController.java
@@ -1,7 +1,75 @@
 package com.incedo.capstone.smartinventory.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.incedo.capstone.smartinventory.entities.Products;
+
+import com.incedo.capstone.smartinventory.exceptions.ProductsNotFoundException;
+import com.incedo.capstone.smartinventory.services.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductsController {
+    @Autowired
+    ProductsService productsService;
+
+    @PostMapping("/addProduct")
+    public ResponseEntity<Products> addProducts(@RequestBody Products products){
+        Products addedProduct=productsService.addProduct(products);
+        return new ResponseEntity<>(addedProduct,HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Products>> getAllProducts(){
+        List<Products> productList= productsService.getAllProducts();
+        if(productList!=null)
+        {
+            return new ResponseEntity<>(productList,HttpStatus.OK);
+
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Products> getProductsById(@PathVariable("id") int id){
+        Products product = productsService.getProductById(id);
+        if (product !=null)
+        {
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Products> updateProduct(@PathVariable("productId") long productId, @RequestBody Products product) {
+        Products updatedProduct = productsService.updateProduct(productId, product);
+        if (updatedProduct != null) {
+            return new ResponseEntity<>(updatedProduct,HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<String> deleteByProductId(@PathVariable("productId") long productId)
+    {
+        try{
+            //String message= productsService.deleteProduct(productId);
+            String message=productsService.deleteProductById(productId);
+            return new ResponseEntity<>(message, HttpStatus.OK);
+        }
+        catch(ProductsNotFoundException infe)
+        {
+            return new ResponseEntity<>(infe.getMessage(), HttpStatus.OK);
+        }
+    }
 }
--- src/main/java/com/incedo/capstone/smartinventory/services/ProductsService.java
+++ src/main/java/com/incedo/capstone/smartinventory/services/ProductsService.java
@@ -1,7 +1,62 @@
 package com.incedo.capstone.smartinventory.services;
 
+import com.incedo.capstone.smartinventory.entities.Products;
+
+import com.incedo.capstone.smartinventory.exceptions.ProductsNotFoundException;
+import com.incedo.capstone.smartinventory.repository.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductsService {
+    @Autowired
+    ProductsRepository productsRepository;
+
+    public Products addProduct(Products products){
+        return productsRepository.save(products);
+
+    }
+
+    public Products getProductById(long productId){
+        Optional<Products> op= productsRepository.findById(productId);
+        return op.orElse(null);
+    }
+
+    public Products updateProduct(long id, Products updatedProduct){
+        Products existingProduct = getProductById(id);
+        if (existingProduct != null) {
+
+            existingProduct.setProductName(updatedProduct.getProductName());
+            existingProduct.setPrice(updatedProduct.getPrice());
+            existingProduct.setQuantity(updatedProduct.getQuantity());
+            existingProduct.setStock(updatedProduct.isStock());
+
+            return productsRepository.save(existingProduct);
+        } else {
+            return null;
+        }
+    }
+    public String deleteProductById(long productId) {
+        Optional<Products> op = productsRepository.findById(productId);
+
+        if (op.isPresent()) {
+            Products deleteProducts = op.get();
+            if (deleteProducts.isStock()) {
+                productsRepository.delete(deleteProducts);
+                return "Product deleted successfully";
+            }
+            else {
+                return "Product cannot be deleted as it is not available ";
+            }
+
+        }
+        throw new ProductsNotFoundException("Products not found for id: " + productId);
+    }
+
+    public List<Products> getAllProducts() {
+        return productsRepository.findAll();
+    }
 }
